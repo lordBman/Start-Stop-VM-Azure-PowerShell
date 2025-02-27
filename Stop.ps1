f60ffde4f77e578e4c47ce7aaec7b32db48456f6
Nobel Okelekele <34259322+lordBman@users.noreply.github.com>
2025-02-27 15:57:00 +0000

    Create Stop.ps1
    powershell script that stops the VM

---
+++ Stop.ps1
@@ -0,0 +1,47 @@
+# Input bindings are passed in via param block.
+param($Timer)
+ 
+# Get the current universal time in the default string format.
+$currentUTCtime = (Get-Date).ToUniversalTime()
+ 
+# The 'IsPastDue' property is 'true' when the current function invocation is later than scheduled.
+if ($Timer.IsPastDue) {
+    Write-Host "PowerShell timer is running late!"
+}
+ 
+# Authenticate with Azure using Managed Identity
+Connect-AzAccount -Identity -ErrorAction Stop
+ 
+# Get all VMs in the resource group
+$vmList = Get-AzVM -Status
+ 
+foreach ($vm in $vmList) {
+    #Get name of VM
+    $vmName = $vm.Name
+ 
+    #Get Resource Group of VM
+    $vmResourceGroupName = $vm.ResourceGroupName
+   
+    $powerState = (Get-AzVM -ResourceGroupName $vm.ResourceGroupName -Name $vm.Name -Status).Statuses.Code
+   
+    Write-Output "VM: $vmName - Power State: $powerState"
+   
+    if ($powerState -eq "PowerState/running") {
+        Write-Output "VM $vmName is running. Initiating deallocation..."
+        Stop-AzVM -ResourceGroupName $vmResourceGroupName -Name $vmName -Force
+        Write-Output "VM $vmName has been successfully deallocated."
+    } elseif ($powerState -eq "PowerState/stopped") {
+        Write-Output "VM $vmName is stopped. Initiating deallocation..."
+        Stop-AzVM -ResourceGroupName $vmResourceGroupName -Name $vmName -Force
+        Write-Output "VM $vmName has been successfully deallocated."
+    } elseif ($powerState -eq "PowerState/deallocated") {
+        Write-Output "VM $vmName is already deallocated. No action required."
+    } else {
+        Write-Output "VM $vmName is in state: $powerState. No action taken."
+    }
+}
+ 
+# Get all VMs in the Subscription
+$vmList = Get-AzVM -Status
+ 
+#Stopping the VM with the name and resource group
